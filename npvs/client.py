bb4ed5b1a57555ebd8961af2fea7851daba35d5c
thanhtoan1742 <thanhtoan1742@gmail.com>
2023-04-27 17:01:44 +0700

    fix client (not tested)

--- npvs/client.py
+++ npvs/client.py
@@ -1,4 +1,5 @@
 import socket
+import json
 
 from npvs import rtp, rtsp
 from npvs.common import *
@@ -8,65 +9,75 @@ from npvs.video import VideoAssembler
 
 
 class Client(MediaPlayer):
-    def __init__(self, ip: str, rtsp_port: int, filename: str) -> None:
+    def __init__(self, server_ip: str, server_rtsp_port: int, filename: str) -> None:
         super().__init__()
 
         self.logger = get_logger("client")
 
-        self.ip = ip
-        self.rtsp_port = rtsp_port
         self.filename = filename
 
-        self.rtp_port = 0
-        self.rtsp_socket: socket.socket = None
+        self.server_ip = server_ip
+        self.server_rtsp_port = server_rtsp_port
+        self.rtsp_cseq = 0
+        self.rtsp_session = -1
+        self.rtsp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.rtspSocket.connect((self.server_ip, self.server_rtsp_port))
 
         self.video_assembler = VideoAssembler()
 
-        self.ps_receiver = PsReceiver(self.ip, self.logger)
-        self.cseq = 0
-        self.session = -1
+        self.rtp_socket: socket.socket = None
+        self.client_rtp_port = 0
+        self.ps_receiver: PsReceiver = None
 
     def __del__(self) -> None:
         self.teardown()
         self.rtsp_socket.close()
 
     def send_RTSP_request(self, method: rtsp.Method) -> bool:
-        self.cseq += 1
+        self.rtsp_cseq += 1
 
         request = {
             "method": method,
             "fileName": self.filename,
-            "CSeq": self.cseq,
+            "CSeq": self.rtsp_cseq,
         }
         if method == rtsp.Method.SETUP:
-            request["clientPort"] = self.rtp_port
+            request["clientPort"] = self.client_rtp_port
         else:
-            request["session"] = self.session
+            request["session"] = self.rtsp_session
         message = rtsp.create_request(request)
         self.rtsp_socket.sendall(message.encode())
+        self.logger.info("sent message = %s", json.dumps(request, indent=2))
 
         message = self.rtsp_socket.recv(rtsp.RTSP_MESSAGE_SIZE).decode()
         self.respond = rtsp.parse_response(message)
         if self.respond["statusCode"] > 299:
-            log(self.respond["statusCode"], "server responded")
+            self.logger.error(
+                "server responded, status code = %s,\nreponsed = %s",
+                self.respond["statusCode"],
+                json.dumps(self.respond, indent=2),
+            )
             return False
 
         return True
 
     def _setup_(self) -> bool:
+        # TODO: don't need to make new socket every time we initiate a
+        # RTP connection.
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind(("", 0))
-        self.rtp_port = int(s.getsockname()[1])  # get the port
+        self.client_rtp_port = int(s.getsockname()[1])  # get the port
         s.listen()
 
         if not self.send_RTSP_request(rtsp.Method.SETUP):
-            self.rtsp_socket.close()
+            s.close()
             return False
 
-        self.session = self.respond["session"]
-        self.rtsp_socket, _ = s.accept()
-        self.rtsp_socket.settimeout(0.5)
+        self.rtsp_session = self.respond["session"]
+        self.rtp_socket, _ = s.accept()
+        self.ps_receiver = PsReceiver(self.rtp_socket, self.logger)
 
+        s.close()
         return True
 
     def _play_(self) -> bool:
@@ -82,12 +93,10 @@ class Client(MediaPlayer):
         return True
 
     def _stream_(self) -> None:
-        if self.packetSizeQueue.empty():
+        payload = self.ps_receiver.next_payload()
+        if not payload:
             return
-
-        size = self.packetSizeQueue.get()
-        data = self.rtpBuffer.readIfEnough(size)
-        self.video_assembler.add_packet(rtp.decode(data))
+        self.video_assembler.add_packet(rtp.decode(payload))
 
     def next_frame(self):
         return self.video_assembler.next_frame()
