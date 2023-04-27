80a1fcb2bec0eedaf40327e5dd39ebc442cb8578
thanhtoan1742 <thanhtoan1742@gmail.com>
2023-04-27 17:06:15 +0700

    remove unnecessary functions and class

--- npvs/common.py
+++ npvs/common.py
@@ -1,15 +1,6 @@
 import logging
 
 
-def log(message: str, header: str = "") -> None:
-    print("-" * 60)
-    if header != "":
-        print(header)
-        print("-" * 40)
-    print(message)
-    print("-" * 60)
-
-
 def split(s: str, delim: str) -> list[str]:
     assert len(delim) == 1
     return list(map(lambda x: x.strip(), s.strip(delim).split(delim)))
