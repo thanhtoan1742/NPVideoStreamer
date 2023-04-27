7b24b58a8240a8ea4db88ef0693b05de7f0dfd1b
thanhtoan1742 <thanhtoan1742@gmail.com>
2023-04-27 14:56:09 +0700

    update to use pytest

--- tests/test_ps.py
+++ tests/test_ps.py
@@ -1,25 +1,27 @@
 from npvs import ps
-import unittest
 
 
-class TestPs(unittest.TestCase):
-    def test_header_value(self):
-        packet = ps.Packet(b"x" * 27)
-        self.assertEqual(packet.header, 27)
+def test_header_value():
+    packet = ps.Packet(b"x" * 27)
+    assert packet.header == 27
 
-    def test_payload_size(self):
-        packet = ps.Packet(b"x" * 27)
-        self.assertEqual(packet.payload_size(), 27)
 
-    def test_payload_value(self):
-        packet = ps.Packet(b"x" * 27)
-        self.assertEqual(packet.payload, b"x" * 27)
+def test_payload_size():
+    packet = ps.Packet(b"x" * 27)
+    assert packet.payload_size() == 27
 
-    def test_encode(self):
-        packet = ps.Packet(b"x" * 27)
-        expect = 0x001B_78787878_78787878_78787878_78787878_78787878_78787878_787878_ED
-        self.assertEqual(packet.encode(), expect.to_bytes(30, "big"))
 
-    def test_decode_header(self):
-        header = (0x001B).to_bytes(2, "big")
-        self.assertEqual(ps.decode_header(header), 27)
+def test_payload_value():
+    packet = ps.Packet(b"x" * 27)
+    assert packet.payload == b"x" * 27
+
+
+def test_encode():
+    packet = ps.Packet(b"x" * 27)
+    expect = 0x001B_78787878_78787878_78787878_78787878_78787878_78787878_787878_ED
+    assert packet.encode() == expect.to_bytes(30, "big")
+
+
+def test_decode_header():
+    header = (0x001B).to_bytes(2, "big")
+    assert ps.decode_header(header) == 27
