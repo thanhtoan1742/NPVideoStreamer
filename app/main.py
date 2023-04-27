cdb4335e02f58caa30bdb89896e86825382cca24
thanhtoan1742 <thanhtoan1742@gmail.com>
2023-04-28 00:18:31 +0700

    add dumper for debug

--- app/main.py
+++ app/main.py
@@ -1,23 +1,40 @@
-from unittest.mock import Mock
-
-from tests.test_ps_receiver import make_recv
-
-s = Mock()
-s.recv.side_effect = make_recv()
-
-
+from npvs import ps
+from npvs.ps_receiver import PsReceiver, BUFFER_SIZE
 from npvs.common import get_logger
-from npvs.ps_receiver import PsReceiver
+from unittest.mock import Mock
+import time
 
-r = PsReceiver(s, get_logger("client"))
-print(r.is_done())
-print(r.next_payload())
-print(r.is_done())
-print(r.next_payload())
-print(r.is_done())
-print(r.next_payload())
-print(r.is_done())
-print(r.next_payload())
-print(r.is_done())
-print(r.next_payload())
-print(r.is_done())
+with open("server-data.bin", "rb") as f:
+    sd = f.read()
+
+with open("client-data.bin", "rb") as f:
+    cd = f.read()
+
+
+def make_socket(data):
+    cur = 0
+
+    def recv(*args):
+        nonlocal cur, data
+        if cur >= len(data):
+            return None
+        nex = min(cur + BUFFER_SIZE, len(data))
+        res = data[cur:nex]
+        cur = nex
+        return res
+
+    socket = Mock()
+    socket.recv.side_effect = recv
+
+    return socket
+
+
+receiver = PsReceiver(make_socket(cd), get_logger("main"))
+while True:
+    if receiver.is_done():
+        break
+    payload = receiver.next_payload()
+    if payload:
+        print(len(payload))
+    else:
+        print(None)
